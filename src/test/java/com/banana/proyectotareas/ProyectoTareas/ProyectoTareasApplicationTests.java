3bc3bb1a6ee15adff03c44c88a9f92bd83cb2824
mariadxc <maria-montserrat.martinez-arcas@dxc.com>
2024-03-03 11:41:16 +0100

    spring initiaalizr

---
+++ src/test/java/com/banana/proyectotareas/ProyectoTareas/ProyectoTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banana.proyectotareas.ProyectoTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
